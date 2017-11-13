66be891827b8c0a622e5355c2d57609905b55d65
limengtaot <871758669@qq.com>
2017-11-13 22:17:28 +0800

    第一次提交

---
+++ src/containers/Home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Home" id="Home">
+      <Home-header></Home-header>
+      <Home-cat></Home-cat>
+  </div>
+</template>
+
+<script>
+
+import HomeHeader from './components/HomeHeader'
+import HomeCat from './components/HomeCat'
+
+export default {
+  name: 'Home',
+  components:{HomeHeader,HomeCat}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped  lang="scss">
+.Home{
+  visibility: visible;
+}
+</style>
